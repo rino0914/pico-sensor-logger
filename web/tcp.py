1afc04f9448f945c8bc3bc99f599164915dfb6ad
rino0914 <rino0914@gmail.com>
2026-07-06 23:57:20 +0900

    소스코드 개선 및 고도화, 구조 변경 진행

--- web/tcp.py
+++ web/tcp.py
@@ -3,14 +3,14 @@ import socket
 
 _SOCKET_CALLBACK_OPTION = 20
 
-
+# @brief TCP 서버 클래스
 class TcpServer:
     def __init__(
         self,
         request_handler,
-        host="0.0.0.0",
-        port=80,
-        backlog=5,
+        host    = "0.0.0.0",
+        port    = 80,
+        backlog = 5,
     ):
         self.request_handler = request_handler
         self.host = host
@@ -18,22 +18,28 @@ class TcpServer:
         self.backlog = backlog
         self.socket = None
 
+    # @brief TCP 서버를 초기화하고 시작
     def start(self):
+        # 1. 소켓이 존재하면 중단
         if self.socket is not None:
             return
-
+        # 2. IP/Port로 주소 생성 및 소켓 할당
         address = socket.getaddrinfo(self.host, self.port)[0][-1]
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
+            # 3. 소켓 옵션 설정
             server_socket.setsockopt(
                 socket.SOL_SOCKET,
                 socket.SO_REUSEADDR,
                 1,
             )
+            # 4. 소켓 Bind -> Listing
             server_socket.bind(address)
             server_socket.listen(self.backlog)
+            # 5. Non-Blocking 모드로 동작
             server_socket.setblocking(False)
+            # 6. 소켓 옵션 생성
             server_socket.setsockopt(
                 socket.SOL_SOCKET,
                 _SOCKET_CALLBACK_OPTION,
@@ -57,8 +63,11 @@ class TcpServer:
         client_socket = None
 
         try:
+            # 7. 소켓 accept wait
             client_socket, client_address = server_socket.accept()
+            # 8. accept 완료시 블록킹 모드로 전환
             client_socket.setblocking(False)
+            # 9. 소켓 옵션 설정
             client_socket.setsockopt(
                 socket.SOL_SOCKET,
                 _SOCKET_CALLBACK_OPTION,
